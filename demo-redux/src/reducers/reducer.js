4b9a418b68121ab50d30dfd7b9359b011c7a59c9
cameron <cameron.grams.2@gmail.com>
2017-12-09 10:36:56 -0500

    trouble shoot

--- demo-redux/src/reducers/reducer.js
+++ demo-redux/src/reducers/reducer.js
@@ -10,6 +10,7 @@ function todos( state = [], action ){
     switch( action.type ){
         case 'ADD_TODO':
           return [ ...state, {
+                id: action.id, 
                 text: action.text,
                 completed: false
              } ];  
